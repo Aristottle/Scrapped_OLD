93a43f4e4acf4f0cc8a17a0a43f0303221d16916
Mason <stottcmason.24@gmail.com>
2023-03-25 00:57:45 -0400

    new speedometer is in and looks great

--- Assets/UI/Scripts/PlayerHUD.cs
+++ Assets/UI/Scripts/PlayerHUD.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHUD : MonoBehaviour
 {
     [HideInInspector] public Gun weapon_ref;
     public TMPro.TextMeshProUGUI ammo_text;
     public TMPro.TextMeshProUGUI speed_text;
+    public RectTransform speedometer_bar;
     public TMPro.TextMeshProUGUI gun_text;
     public GameObject crosshair;
     public Rigidbody rb_ref;
@@ -25,9 +27,12 @@ public class PlayerHUD : MonoBehaviour
 
         // Calc the speed
         Vector2 flat_speed = new Vector2(rb_ref.velocity.x, rb_ref.velocity.z);
-        int speed = Mathf.RoundToInt(flat_speed.magnitude * 3.6f);
+        int speed_num = Mathf.RoundToInt(Mathf.Clamp(flat_speed.magnitude * 3.6f, 0, 50));
+        float speed_scale = Mathf.Clamp01(speed_num / 50f);
         // Set the text
-        speed_text.text = $"{speed} kph";
+        speed_text.text = string.Format("{0:00}<size=12> kph</size>", speed_num);
+        // Set the scale
+        speedometer_bar.localScale = Vector3.Lerp(speedometer_bar.localScale, new Vector3(1, speed_scale, 1), Time.deltaTime * 8);
     }
 
     public void ToggleCrosshair(bool active)
