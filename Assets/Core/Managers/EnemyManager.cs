6242a4bdc6116a64341399ff7b2865cd9464cbe3
Mason Stott <118931015+Aristottle@users.noreply.github.com>
2023-09-21 12:05:58 -0400

    basic enemy spawning

---
+++ Assets/Core/Managers/EnemyManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] private Transform spawnTransform;
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private int numToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!spawnTransform || !enemyPrefab)
+            return;
+
+        for (int i = 0; i < numToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, spawnTransform.position, spawnTransform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
